b2400f67c701170f905f9683920612ecf0d19b49
Youna Yang <ayounayang@gmail.com>
2016-05-24 14:01:30 -0700

    finished hamsters assignment

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,23 +2,29 @@ puts "What is the hamster's name?"
 name = gets.chomp
 
 puts "On a volumne scale of 1 to 10 how loud is " + name + "?"
-loudness = gets.chomp
-puts loudness.to_i
+loudness = gets.chomp.to_i
 
 puts "What's " + name + "\'s fur color?"
 fcolor = gets.chomp
-puts fcolor
 
-puts "Is " + name + " a good candidate for adoption? [y/n]"
+puts "Is " + name + " a good candidate for adoption? [yes/no]"
 lovable = gets.chomp
-puts lovable
 
-puts "What is " + name + "\'s estimated age?"
-age = gets.strip
+puts "What is #{name}'s estimated age?"
+age = gets.chomp
 if age.empty?
   age = nil
 else
   age = age.to_i
 end
 
-p age
+
+puts "Hamster Information"
+puts "Hamster name: " + name
+#puts "Volumne level: " + loudness
+puts "Fur color: " + fcolor
+puts "Good for adoption: " + lovable
+puts "Estimated age: #{age}"
+
+# another way to print the estimated age puts "Estimated age: " + age.to_s
+# without using string interpolation. JS doesn't have string interpolation so will have to use tht + sign

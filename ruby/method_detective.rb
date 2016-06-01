e6b6a21f3880401f154a1c223e2990e0b430f208
Youna Yang <ayounayang@gmail.com>
2016-05-31 18:19:42 -0700

    finished method detective assignment

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,67 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+#########
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
+"OoNaMaNaPiA".swapcase
+# => "oOnAmAnApIa"
 
-# "zom".<???>
+#########
+"zom".insert(1, 'o')
 # => “zoom”
+"waterellon".insert(5, 'm')
 
-# "enhance".<???>
+#########
+"enhance".center(15)
 # => "    enhance    "
+"mississippi".center(20)
+# => "    mississippi     "
 
-# "Stop! You’re under arrest!".<???>
+#########
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
+"Don't stop believin'".upcase
+# => "DON'T STOP BELIEVIN'"
 
-# "the usual".<???>
+#########
+"the usual".concat(" suspects")
 #=> "the usual suspects"
+"I believe".concat(" I can fly")
+#=> "I believe I can fly"
 
-# " suspects".<???>
+#########
+" suspects".prepend("the usual")
 # => "the usual suspects"
+"world".prepend("hello ")
+# => "hello world"
 
-# "The case of the disappearing last letter".<???>
+#########
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
+"Desserts".chop
+# => "Dessert"
 
-# "The mystery of the missing first letter".<???>
+#########
+"The mystery of the missing first letter"[1..-1]
 # => "he mystery of the missing first letter"
+"Hello"[1..-1]
+# => "ello"
 
-# "Elementary,    my   dear        Watson!".<???>
+#########
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
+"We    don't  want      repeating     spaces".squeeze(" ")
+# => "We don't want repeating spaces"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+#########
+"z".ord
+# => 122
+"o".ord
+# => 111
 
-# "How many times does the letter 'a' appear in this string?".<???>
+#########
+"How many times does the letter 'a' appear in this string?".count('a')
 # => 4
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer".count('e')
+# => 7

03a634aa7676506a2352396815e0a5259311e5d9
Youna Yang <ayounayang@gmail.com>
2016-06-07 15:59:42 -0700

    add attributes at top

---
+++ ruby/puppy.rb
@@ -0,0 +1,41 @@
+SPECIFIES --------------------
+Puppy
+
+CHARACTERISTICS --------------
+Color : Light brown
+Fur length : 1 inch
+Sheds : no
+Tail length : 2 inch
+Personality : lively
+
+BEHAVIOR ---------------------
+Cuddle
+Bark
+Whine
+Singz
+
+=begin
+# create new instance of puppy class. Instances:Class :: Members:Species
+class Puppy
+  p spot.class # Puppy
+  p duchess == fido #false
+  p fido.instance_of?(Array) #false
+  p fido.instance_of?(Puppy) #true
+# p spot.play_dead #undefined
+end
+
+duchess = Puppy.new # creating new instance
+fido = Puppy.new
+spot = Puppy.new
+
+p str = String.new # ""
+p str.length # 0
+p str.empty? # true
+p str += "huh." #"huh."
+p str = "some string" # "some string"
+p greeting = String.new("hello") #{}"hello"
+p greeting.length # 5
+p greeting.upcase #"HELLO"
+
+=end
+

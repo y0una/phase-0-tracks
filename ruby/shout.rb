17516f020c6c2aca78fb4027a6fd82b71165cdf0
Youna Yang <ayounayang@gmail.com>
2016-06-08 22:33:04 -0700

    release 1 done

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,7 +1,13 @@
 module Shout
   # we'll put some methods here soon, but this code is fine for now!
   def self.yell_angrily(words)
-    words + "!!!" + " :("
+    puts "#{words}" + "!!!" + " :("
+  end
+
+  def self.yelling_happily(joy_yells)
+    puts "Yells of joy like #{joy_yells}"
   end
 end
 
+Shout.yell_angrily("Arrrrggghhhh")
+Shout.yelling_happily("Wooohooo!")

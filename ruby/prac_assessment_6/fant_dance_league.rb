d7aa02b56482e25f291d8e23b30bb285bbfdc9db
Youna Yang <ayounayang@gmail.com>
2016-06-09 17:12:03 -0700

    initial commit

---
+++ ruby/prac_assessment_6/fant_dance_league.rb
@@ -0,0 +1,16 @@
+class League
+#attr_reader :name
+#attr_accessor :age
+  def initialize
+    @name = name
+    @age = age
+  end
+
+  def name
+    @name
+  end
+
+  def age(age)
+    @age = age
+  end
+end
---
+++ ruby/prac_assessment_6/fant_dance_league_spec.rb
@@ -0,0 +1,18 @@
+require_relative 'fant_dance_league'
+
+describe Dancer do
+  let(:dancer) { Dancer.new("Misty Copeland", 33) }
+
+  it "has a readable name" do
+    expect(dancer.name).to eq "Misty Copeland"
+  end
+
+  it "has a readable age" do
+    expect(dancer.age).to eq 33
+  end
+
+  it "has a writeable age" do
+    dancer.age = 34
+    expect(dancer.age).to eq 34
+  end
+end

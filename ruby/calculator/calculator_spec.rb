2d5e4f6d989e79459f356eb72b22429461164b49
Youna Yang <ayounayang@gmail.com>
2016-06-10 13:39:34 -0700

    finished calculator and spec files

--- ruby/calculator/calculator_spec.rb
+++ ruby/calculator/calculator_spec.rb
@@ -4,14 +4,14 @@
 # multiply two integers and return an integer
 
 # RSpec
-# 'describe' block for each group of tests  
+# 'describe' block for each group of tests
   # 'it' block for each individual test
     # expect(<YOUR CODE>).to eq <RESULT>
 
 require_relative 'calculator'
 
 describe Calculator do
-  let(:calculator) { Calculator.new }
+  let(:calculator) { Calculator.new } # removes from each individual block below
 
   it "adds two integers" do
     expect(calculator.add(3,4)).to eq 7
@@ -23,5 +23,9 @@ describe Calculator do
 
   it "multiplies two integers" do
     expect(calculator.multiply(2,3)).to eq 6
+  end
+
+  it "divides two integers" do
+    expect(calculator.divide(16,4)).to eq 4
   end
 end

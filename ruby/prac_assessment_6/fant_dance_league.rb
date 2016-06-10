bc65e6b9f489195f8a2b0bf3d4417461f418f451
Youna Yang <ayounayang@gmail.com>
2016-06-10 13:23:26 -0700

    complete

--- ruby/prac_assessment_6/fant_dance_league.rb
+++ ruby/prac_assessment_6/fant_dance_league.rb
@@ -1,16 +1,34 @@
-class League
-#attr_reader :name
-#attr_accessor :age
-  def initialize
+class Dancer
+attr_reader :name
+attr_accessor :age
+
+  def initialize(name, age)
     @name = name
     @age = age
+    @card = Array.new
   end
 
-  def name
-    @name
+  def pirouette
+    "*twirls*"
   end
 
-  def age(age)
-    @age = age
+  def bow
+    "*bows*"
+  end
+
+  def queue_dance_with(partner)
+    @card << partner
+  end
+
+  def card
+    @card
+  end
+
+  def begin_next_dance
+    "Now dancing with #{@card.delete_at(0)}."
+  end
+
+  def stretch
+    "*stretch*"
   end
 end

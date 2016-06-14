a1ca789cc86235f6db60fd7fcb7ca8701db1387c
Youna Yang <ayounayang@gmail.com>
2016-06-14 16:14:25 -0700

    first part

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["teal", "burnt sienna", "aquamarine blue", "yellow"];
+var horseName = ["Amanda", "Anabel", "Nibisco", "Nickles and Dimes"];
+
+colors.push("red");
+horseName.push("Nibby");
+
+console.log(colors);
+console.log(horseName);
